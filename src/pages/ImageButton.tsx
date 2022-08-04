4655fcca5148f7622ae627c82fa36618f26bee92
Bharadwaj Palakurthy <bharadwajpalakurthy@gmail.com>
2022-08-04 06:33:46 +0530

    refactor + store + loading

---
+++ src/pages/ImageButton.tsx
@@ -0,0 +1,55 @@
+import { useEthers } from "@usedapp/core";
+import { ETHMainnet } from "constants/networks";
+import { useNetworkInfo } from "stores/networkInfo";
+
+interface IWallet {
+    image?: string;
+    name: string;
+    onSelect?: (value: any) => void;
+  }
+  
+  export const ImageButton = ({ image, name }: IWallet) => {
+
+  const { activateBrowserWallet, switchNetwork } = useEthers();
+  const networkInfo = useNetworkInfo();
+
+      return (
+
+          <div
+              className="image-button button"
+              onClick={async () => {
+                  //1 for ethereum mainnet, 15 for gravity bridge testnet
+                  activateBrowserWallet();
+
+                  if (Number(networkInfo.chainId) != 1)
+                      switchNetwork(ETHMainnet.chainId);
+              } }
+              style={{
+                  backgroundColor: "#1C1C1C",
+                  padding: "1rem 1.4rem",
+                  color: "white",
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "1rem",
+                  width: "20rem",
+                  cursor: "pointer",
+              }}
+          >
+              {image ? <img src={image} alt={name} height={24} width={24} /> : null}
+              <span
+                  style={{
+                      flex: "2",
+                      textAlign: "center",
+                  }}
+              >
+                  {networkInfo.account
+                      ? Number(networkInfo.chainId) != 1
+                          ? "switch to ethereum network"
+                          : networkInfo.account.substring(0, 10) +
+                          "..." +
+                          networkInfo.account.substring(networkInfo.account.length - 10, networkInfo.account.length)
+                      : "connect"}
+              </span>
+          </div>
+      );
+  };
--- src/stores/tokens.tsx
+++ src/stores/tokens.tsx
@@ -1,27 +1,77 @@
 import create from "zustand";
 import { devtools } from "zustand/middleware";
 import emptyToken from "assets/empty.svg";
+import { ChainId, TransactionState, TransactionStatus } from "@usedapp/core";
+import { providers } from "ethers";
+import { GTokens } from "hooks/useGravityTokens";
+
+interface TxStatus {
+    state: TransactionStatus;
+    send: (...args: any[]) => Promise<providers.TransactionReceipt | undefined>;
+    resetState: () => void;
+}
 
 interface TokenStore {
-    tokens : any[],
-    setTokens : (tokens : any[]) => void,
-    selectedToken : any,
-    setSelectedToken : (token : any) => void
+  network : {
+    chainID : ChainId | undefined,
+    setChainID : (value  : ChainId) => void
+  };
+  tokens: GTokens[];
+  setTokens: (tokens: GTokens[]) => void;
+  selectedToken: GTokens;
+  setSelectedToken: (token: any) => void;
+  approveStatus: TransactionState ;
+  setApproveStatus : (tx : TransactionState) => void;
+  cosmosStatus: TransactionState;
+  setCosmosStatus : (tx : TransactionState) => void;
+
+}
+export const useTokenStore = create<TokenStore>()(
+  devtools((set, get) => ({
+    network :{
+      chainID : undefined,
+      setChainID : (val) => set(
+        {
+          network : {
+            chainID : val,
+            setChainID : get().network.setChainID
+          }
+        }
+      )
+    },
+    tokens: [],
+    setTokens: (tokens: GTokens[]) => set({ tokens: tokens }),
+    selectedToken: {
+      data : {
+      isERC20: false,
+      isLP: false,
+      symbol: "NOTHIN",
+      address: "0x0412C7c846bb6b7DC462CF6B453f76D8440b2609",
+      cTokenAddress: "",
+      decimals: 0,
+      icon: emptyToken,
+      name: "select token"
+    },
+      wallet : "",
+      allowance: -1,
+      balanceOf: -1,
+    },
+    setSelectedToken: (token: GTokens) => {
+      set({
+        selectedToken: token,
+      });
+    },
+    approveStatus: "None",
+    setApproveStatus : (tx) => set({
+      approveStatus : tx
+    }),
+    cosmosStatus: "None",
+    setCosmosStatus : (tx) => set({
+      cosmosStatus : tx
+    }),
   }
   
-  export const useTokenStore = create<TokenStore>()(
-    devtools((set) => ({
-        tokens : [],
-        setTokens : (tokens : any[]) => set({tokens: tokens}),
-        selectedToken : {
-            data: {
-                icon: emptyToken,
-                name: "select token",
-                address: "0x0412C7c846bb6b7DC462CF6B453f76D8440b2609",
-              },
-              allowance: -1,
-              balanceOf: -1,
-        },
-        setSelectedToken : (token : any) => set({selectedToken : token})
-    }))
-  );
+  ))
+);
+
+
