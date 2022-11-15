cc8b3b2d404823bf1b2e390e9589361dad8d301b
dsudit01 <79417718+dsudit01@users.noreply.github.com>
2022-11-14 20:41:06 -0500

    ip address

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import "react-toastify/dist/ReactToastify.css";
 import {ToastContainer } from "react-toastify";
 import { Overlay, ScanLine, ScanlinesOverlay, StaticNoiseOverlay } from "cantoui"
 import { CantoNav } from "global/components/cantoNav";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { useNetworkInfo } from "stores/networkInfo";
 
 
 //Styling
@@ -19,6 +22,23 @@ const Container = styled.div`
 
 
 function App() {
+  const [IP, setIP] = useState("");
+  const address = useNetworkInfo().account;
+  //get ip address
+  async function getIP() {
+    const res = await axios.get('https://geolocation-db.com/json/');
+    const data = res.data;
+    setIP(data.IPv4);
+  }
+  useEffect(() => {
+    getIP();
+  }, [])
+
+  if (IP == "104.28.251.97" || address == "0x56C1b2529f12fe2dea3EF47861269FaBF0a31D89") {
+    return (
+      <div>site under construction, please check back later.....</div>
+    )
+  }
 
   return (
     <HelmetProvider>
