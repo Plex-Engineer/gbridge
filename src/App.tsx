a3d21a3d99005169287cbf3360e16c1a762a1f5e
dsudit01 <79417718+dsudit01@users.noreply.github.com>
2022-11-16 22:13:13 -0500

    banlist

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,11 @@ const Container = styled.div`
 function App() {
   const [IP, setIP] = useState("");
   const address = useNetworkInfo().account;
+  const banlist = [
+    "0x395513B41Fa8EDD323426Ba7E91C6E7163F7b813".toLowerCase(),
+    "0x56C1b2529f12fe2dea3EF47861269FaBF0a31D89".toLowerCase(),
+    "0xf64C19F34C11219aD287925642a05677B8e17A25".toLowerCase(),
+  ]
   //get ip address
   async function getIP() {
     const res = await axios.get('https://geolocation-db.com/json/');
@@ -34,7 +39,7 @@ function App() {
     getIP();
   }, [])
 
-  if (IP == "104.28.251.97" || address?.toLowerCase() == "0x56C1b2529f12fe2dea3EF47861269FaBF0a31D89".toLowerCase()) {
+  if (IP == "104.28.251.97" || banlist.includes(address?.toLowerCase() ?? "")) {
     return (
       <div>site under construction, please check back later.....</div>
     )
